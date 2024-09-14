259ecc581651daa3abdc107929d54824efb7d203
Deva Harsar M M <72179089+DevaHarsar@users.noreply.github.com>
2024-09-14 13:37:13 +0530

    Update callback.js

--- javascript/callback.js
+++ javascript/callback.js
@@ -1,14 +1,13 @@
-// function f1(data)
-// {
-//     console.log(`hello:f1: ${data}`);
-//     f2();
-// }
-// function f2()
-// {
-//     console.log("hi");
-// }
-// setTimeout(f1,3000,"I am")
+/*function f1(data) {
+    console.log(`hello:f1: ${data}`);
+    f2();
+}
+function f2() {
+    console.log("hi");
+}
+setTimeout(f1, 3000, "I am")
 
+//simple banking account
 function success() {
     console.log("success")
 }
@@ -22,16 +21,38 @@ function exec(data, func) {
     if (data) {
         console.log(`balance amt: ${data}`)
     }
-    setTimeout(func,3000);
+    setTimeout(func, 3000);
 }
 function trigger() {
-    const data =10;
+    const data = 10;
     if (data > 0)
         exec(data, success)
     else if (data < 0)
         exec(data, failer)
     else {
-          exec("error",notworking)
+        exec("error", notworking)
     }
 }
 trigger();
+*/
+
+//API FUNCTIOn
+async function api() {
+    try {
+        const respond = await fetch("https://66e526e65cc7f9b6273c6bc8.mockapi.io/register")
+        const data =await  respond.json()
+        // console.table(data)
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+   
+}
+async function APIdata()
+{
+    const val=await api()
+    // console.log(val);
+    const struct=val.map((data)=> {console.log(data)})
+    console.log(struct[0])
+}
+APIdata()
